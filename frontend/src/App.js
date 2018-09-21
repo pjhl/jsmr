d87905671c9a0739a76ba91bce03af967b5a1275
pjhl <pjhl.coder@gmail.com>
2018-09-21 19:14:13 +0300

    Added the react-router-dom and examples

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react' // , { Component }
 import './App.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Topics from './components/Topics'
 
-class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        Here is an App container
-      </div>
-    )
-  }
-}
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/topics'>Topics</Link></li>
+      </ul>
 
-export default App
+      <hr />
+
+      <Route exact path='/' component={Home} />
+      <Route path='/about' component={About} />
+      <Route path='/topics' component={Topics} />
+    </div>
+  </Router>
+)
+export default BasicExample
